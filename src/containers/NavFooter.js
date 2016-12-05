9e213fcda5f1fd5e14610e87c5d07f84aa19142f
orangeC <18332563755@163.com>
2016-12-05 21:04:19 +0800

    navfooter

--- src/containers/NavFooter.js
+++ src/containers/NavFooter.js
@@ -10,30 +10,55 @@ import img7 from "../../images/littlepic/7.png";
 import img8 from "../../images/littlepic/8.png";
 import img9 from "../../images/littlepic/9.png";
 class NavFooter extends React.Component {
-  
-  handleTabsChange(to){
-    this.context.router.push(to)
+  constructor(){
+    super();
+    this.state={
+      switch:true
+    }
   }
+  
   render () {
     let styles={
-      img3:{
-        marginTop:"0.7rem"
+      imgA:{
+        display: this.state.switch == 1 ? "none" : "block"
+      },
+      imgB:{
+        display: this.state.switch == 2 ? "none" : "block"
+      },
+      imgC:{
+        display: this.state.switch == 3 ? "none" : "block"
+      },
+      imgD:{
+        display: this.state.switch == 4 ? "none" : "block"
       }
     }
     
     return(
       <div className="navfooter">
-            <Link to="/" activeStyle={{color:"#fff"}} onlyActiveOnIndex={true}><span><img src={img8} /><img src={img9} className="none" /></span></Link>
-            <Link to="/recommend" activeStyle={{color:"#fff"}}><span><img src={img7} /><img src={img6} className="none" /></span></Link>
-            <Link to="/publish" activeStyle={{color:"#fff"}}><span><img src={img3} style={styles.img3} /><img style={styles.img3} src={img2} className="none" /></span></Link>
-            <Link to="/personal" activeStyle={{color:"#fff"}}><span><img src={img5} /><img src={img4} className="none" /></span></Link>
+            <Link to="/" activeStyle={{color:"#fff"}} onlyActiveOnIndex={true}>
+              <div onClick={ () => this.setState({switch:1}) }>
+              <img style={styles.imgA}  src={img8} className="one" />
+              <img   src={img9} className="two" /></div>
+            </Link>
+            <Link to="/recommend" activeStyle={{color:"#fff"}}>
+              <div onClick={ () => this.setState({switch:2}) }>
+              <img style={styles.imgB}  src={img7} className="three" />
+              <img   src={img6} className="four" /></div>
+            </Link>
+            <Link to="/publish" activeStyle={{color:"#fff"}}>
+              <div onClick={ () => this.setState({switch:3}) }>
+              <img style={styles.imgC}  src={img3} className="five" />
+              <img   src={img2} className="six" /></div>
+            </Link>
+            <Link to="/personal" activeStyle={{color:"#fff"}}>
+              <div onClick={ () => this.setState({switch:4}) }>
+              <img style={styles.imgD}  src={img5} className="seven" />
+              <img   src={img4} className="eight" /></div>
+            </Link>
       </div>
     )
   }
 }
 
-NavFooter.contextTypes={
-  router:React.PropTypes.object.isRequired
-}
 
 export default NavFooter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
+
 
 import Routes from './routes.js';
 import injectTapEventPlugin from 'react-tap-event-plugin';

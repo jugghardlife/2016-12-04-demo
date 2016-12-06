0933b286927190f9dc21e69f50466b34baf98569
orangeC <18332563755@163.com>
2016-12-06 15:21:58 +0800

    a

--- src/component/Show.js
+++ src/component/Show.js
@@ -2,19 +2,16 @@ import React from 'react';
 import {Link} from 'react-router';
 import Shuffling from './shuffling';
 import Login from './login';
+import Top from '../containers/Top';
+import Center from '../containers/Center';
 
 class Show extends React.Component {
   render () {
-  	let styles={
-  		root:{
-  			display:"flex",
-  			flexDirection:"column",
-  			flexWrap:"wrap"
-  		}
-  	}
     return(
       <div>
-      	<Login />
+      <div>
+        <Top />
+      </div>
       </div>
     )
   }

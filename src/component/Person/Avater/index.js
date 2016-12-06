b933cf39c72a58b2b6e21fe55b54f93724e09f78
richardjoin <2547900199@qq.com>
2016-12-06 13:42:43 +0800

    ly06-1

---
+++ src/component/Person/Avater/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Avater extends React.Component {
+  handleClick(){
+    alert('aaa')
+  }
+  handleAvater(){
+    alert('bbb')
+  }
+  render () {
+    return(
+      <div className='Personal-wrap'>
+        <div className='head-wrap'>
+          <span className="glyphicon glyphicon-cog" onClick={this.handleClick.bind(this)}></span>
+          <img onClick={this.handleAvater.bind(this)} className='st-wrap' />
+          <h3 className='st-wrapp'>阴雨天</h3>
+          <div className='st-wrappp'>
+            <div><span>关注</span><br/><span>276</span></div>
+            <div><span>发布</span><br/><span>243</span></div>
+            <div><span>访客</span><br/><span>256</span></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Avater;

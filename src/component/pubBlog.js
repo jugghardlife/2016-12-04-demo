1e22ad9418b1500f1bfc6126a7b2423bb92184af
liuxicui <870566031@qq.com>
2016-12-06 15:12:27 +0800

    publish f

--- src/component/pubBlog.js
+++ src/component/pubBlog.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
 import '../publish.css';
-
-
 class PubBlog extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      value:''
-    }
-  }
-  handleChange(){
-    this.props.handleSubmit();
-  }
   render () {
     return(
-      <div className='pubBlog-wrap' onChange={this.handleChange.bind(this)}>发布成功！</div>
+      <div>发布成功！</div>
     )
   }
 }
